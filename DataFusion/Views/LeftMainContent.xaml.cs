3ff936136ab496dd18b796b940ceb678cc5b6646
Administrator <Administrator@GLTECH>
2020-03-06 16:07:48 +0800

    changge Views

--- DataFusion/Views/LeftMainContent.xaml.cs
+++ DataFusion/Views/LeftMainContent.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace DataFusion.UserControls
+namespace DataFusion.Views
 {
     /// <summary>
     /// LeftMainContent.xaml 的交互逻辑
